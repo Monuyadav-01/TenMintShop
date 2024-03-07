2683079a7fd4217ee1181e554c2c1a427e2d9596
monu <monuyadav10012000@gmail.com>
2024-03-07 16:29:41 +0530

    new project

--- app/src/main/java/com/example/tenmintshop/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/example/tenmintshop/fragment/home/HomeFragment.kt
@@ -5,22 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.tenmintshop.R
+import com.example.tenmintshop.adapters.AdapterCategory
 import com.example.tenmintshop.databinding.FragmentHomeBinding
+import com.example.tenmintshop.models.Category
+import com.example.tenmintshop.utils.Constants
 import com.example.tenmintshop.utils.Utils
 
 class HomeFragment : Fragment() {
 
-private lateinit var binding: FragmentHomeBinding
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-       binding = FragmentHomeBinding.inflate(inflater, container , false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        Utils.setStatusBarColor(requireContext() , requireActivity())
-        return  binding.root
+        Utils.setStatusBarColor(requireContext(), requireActivity())
+
+        setAllCategories()
+        return binding.root
+    }
+
+    private fun setAllCategories() {
+        val categoryList = ArrayList<Category>()
+        for (i in 0 until Constants.allProductsCategoryIcon.size) {
+            categoryList.add(
+                Category(
+                    Constants.allProductsCategory[i],
+                    Constants.allProductsCategoryIcon[i]
+                )
+            )
+        }
+        binding.rvCategories.adapter = AdapterCategory(categoryList)
     }
 
 }
---
+++ app/src/main/java/com/example/tenmintshop/utils/Constants.kt
@@ -0,0 +1,53 @@
+package com.example.tenmintshop.utils
+
+import com.example.tenmintshop.R
+
+object Constants {
+
+    val allProductsCategory = arrayOf(
+        "Vegetables & Fruits",
+        "Dairy & BreakFast",
+        "Munchies",
+        "ColdDrinks",
+        "Instant & Frozen Food",
+        "Tea Coffee & Heath Drink",
+        "Bakery & Biscuits",
+        "Sweet Tooth",
+        "Atta Rice & Dal",
+        "DryFruits Masala & Oil",
+        "Sauces",
+        "Chicken",
+        "pan corner",
+        "organic & premium",
+        "Baby care",
+        "Pharma & Wellness",
+        "Cleaning",
+        "Home & office",
+        "personal Care",
+        "Pet Care"
+
+    )
+
+    val allProductsCategoryIcon = arrayOf(
+        R.drawable.vegetable,
+        R.drawable.dairy_breakfast,
+        R.drawable.munchies,
+        R.drawable.cold_and_juices,
+        R.drawable.instant_frozen,
+        R.drawable.tea_coffee,
+        R.drawable.bakery_biscuits,
+        R.drawable.sweet_tooth,
+        R.drawable.atta_rice,
+        R.drawable.dry_masala,
+        R.drawable.sauce_spreads,
+        R.drawable.chicken_meat,
+        R.drawable.paan_corner,
+        R.drawable.organic_premium,
+        R.drawable.baby_care,
+        R.drawable.pharma_wellness,
+        R.drawable.cleaning,
+        R.drawable.home_office,
+        R.drawable.personal_care,
+        R.drawable.pet_care
+    )
+}
